90c9bf9b9f905852995dacf2fe53438831d48456
Oscar Arvidsson <oscar.arvidsson@protonmail.com>
2025-12-04 13:46:45 +0100

    added gitignore

--- optimizer.c
+++ optimizer.c
@@ -5,3 +5,7 @@
 void OptimizerOptimize(Optimizer o, struct vecArr optParams, struct vecArr dOptParams) {
     o->optimize(o, optParams, dOptParams);
 }
+
+Optimizer OptimizerInitCopy(Optimizer o, struct vecArr optParams) {
+    return o->initCopy(o, optParams);
+}
